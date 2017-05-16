83691069a334cfe9345ac07ced0a3a33ea5658fd
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2017-05-16 16:24:08 -0400

    First Commit

---
+++ src/main/java/io/zipcoder/fundamental/program/structures/exercise1/IntegerPrinter.java
@@ -0,0 +1,21 @@
+package io.zipcoder.fundamental.program.structures.exercise1;
+
+
+public class IntegerPrinter {
+
+    public String printIntegerAsBinary(int value){
+        return null;
+    }
+
+    public String printIntegerAsOctal(int value){
+        return null;
+    }
+
+    public String printIntegerAsHexadecimal(int value){
+        return null;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/io/zipcoder/fundamental/program/structures/exercise1/README.md
@@ -0,0 +1,6 @@
+#Exercise 1
+
+Write a program that reads an integer and prints it in :
+1. binary
+2. Octal
+3. Hexadecimal
