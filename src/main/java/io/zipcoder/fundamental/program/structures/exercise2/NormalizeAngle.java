5ec5ec414762c89ae0e4c8ef0c67d238e5c252ef
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2017-05-16 16:44:16 -0400

    Added exercise 2

---
+++ src/main/java/io/zipcoder/fundamental/program/structures/exercise2/NormalizeAngle.java
@@ -0,0 +1,17 @@
+package io.zipcoder.fundamental.program.structures.exercise2;
+
+
+public class NormalizeAngle {
+
+    public Integer normalizeValueUsingModulo(Integer angle){
+        return 0;
+    }
+
+    public Integer normalizeValueUsingFloorMod(Integer integer){
+        return 0;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
